b646e31977bad7e14e6c55a398d34825cc98655d
An <anacelol1234@gmail.com>
2025-03-08 14:45:32 +0700

    init

--- apps/jobber-auth/jest.config.ts
+++ apps/jobber-auth/jest.config.ts
@@ -1,10 +1,13 @@
 export default {
-  displayName: 'jobber-auth',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/jobber-auth',
+    displayName: 'jobber-auth',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/apps/jobber-auth',
 };
