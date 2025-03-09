0b71e27232574d5d526233ddf2972b14d7ee9163
An <anacelol1234@gmail.com>
2025-03-09 19:59:32 +0700

    implementing Graphql with UserService to create simple CRUD

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
+import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [
+        PrismaModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: true,
+        }),
+        UsersModule,
+    ],
     controllers: [],
     providers: [],
 })
