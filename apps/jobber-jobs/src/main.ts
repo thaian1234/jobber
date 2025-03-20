c82babef1b4dffc34c1d6aa81ee99ccb6e34f1e4
An <anacelol1234@gmail.com>
2025-03-20 13:14:39 +0700

    implementing jobber-jobs application

---
+++ apps/jobber-jobs/src/main.ts
@@ -0,0 +1,30 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = 'api';
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+        })
+    );
+
+    app.setGlobalPrefix(globalPrefix);
+    app.use(cookieParser());
+    const port = app.get(ConfigService).getOrThrow('PORT') || 3000;
+    await app.listen(port);
+    Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
+}
+
+bootstrap();
